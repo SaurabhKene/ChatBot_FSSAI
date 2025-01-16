5dbc588605a76f0f0366d51b48ee2d3f4d2dfa60
SaurabhKene <saurabhkene150@gmail.com>
2025-01-16 15:31:16 +0530

    Track Application updated

--- src/component/ChatButton.js
+++ src/component/ChatButton.js
@@ -1,56 +1,56 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
-import BotIconImg from "../Images/chaticon2.png";
+import BotIconImg from "../assest/Images/chaticon2.png";
 
-  const BotIcon = () => {
-    return (
-      <BotIconContainer>
-        <BotLink href="#" title="FSSAI Personal Assistance">
-          <SvgIcon
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            x="0px"
-            y="0px"
-            viewBox="0 0 100 100"
-            enableBackground="new 0 0 100 100"
-            xmlSpace="preserve"
-          >
-            {/* Image does not rotate */}
-            <image
-              x="19"
-              y="15"
-              width="70"
-              height="70"
-              id="imglogo"
-              href={BotIconImg}
+const BotIcon = () => {
+  return (
+    <BotIconContainer>
+      <BotLink href="#" title="FSSAI Personal Assistance">
+        <SvgIcon
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          viewBox="0 0 100 100"
+          enableBackground="new 0 0 100 100"
+          xmlSpace="preserve"
+        >
+          {/* Image does not rotate */}
+          <image
+            x="19"
+            y="15"
+            width="70"
+            height="70"
+            id="imglogo"
+            href={BotIconImg}
+          />
+
+          {/* Only the path and text rotate */}
+          <g className="rotate-group">
+            <path
+              id="textPath"
+              fill="none"
+              d="M89.322,50.197c0,22.09-17.91,40-40,40c-22.089,0-40-17.91-40-40 c0-22.089,17.911-40,40-40C71.412,10.197,89.322,28.108,89.322,50.197z"
             />
+            <text style={{ fill: "#fc6748", fontSize: "11px" }}>
+              <textPath xlinkHref="#textPath">
+                <tspan x="0">Assuring Safe & Nututious Food,</tspan>
+                <tspan x="145" dx="25">
+                  Inspring Trust
+                </tspan>
+              </textPath>
+            </text>
+          </g>
+        </SvgIcon>
+      </BotLink>
+    </BotIconContainer>
+  );
+};
 
-            {/* Only the path and text rotate */}
-            <g className="rotate-group">
-              <path
-                id="textPath"
-                fill="none"
-                d="M89.322,50.197c0,22.09-17.91,40-40,40c-22.089,0-40-17.91-40-40 c0-22.089,17.911-40,40-40C71.412,10.197,89.322,28.108,89.322,50.197z"
-              />
-              <text style={{ fill: "#fc6748", fontSize: "11px" }}>
-                <textPath xlinkHref="#textPath">
-                  <tspan x="0">Assuring Safe & Nututious Food,</tspan>
-                  <tspan x="145" dx="25">
-                    Inspring Trust
-                  </tspan>
-                </textPath>
-              </text>
-            </g>
-          </SvgIcon>
-        </BotLink>
-      </BotIconContainer>
-    );
-  };
+export default BotIcon;
 
-  export default BotIcon;
-
-  const rotate = keyframes`
+const rotate = keyframes`
     from {
       transform: rotate(0deg);
     }
@@ -59,30 +59,30 @@ import BotIconImg from "../Images/chaticon2.png";
     }
   `;
 
-  const BotLink = styled.a`
-    display: block;
-  `;
+const BotLink = styled.a`
+  display: block;
+`;
 
-  const BotIconContainer = styled.div`
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
-    width: 100px;
-    height: 100px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
+const BotIconContainer = styled.div`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-  const SvgIcon = styled.svg`
-    transform-origin: 50% 50%; /* Center the rotation */
-    width: 120px;
-    height: 120px;
-    display: block;
+const SvgIcon = styled.svg`
+  transform-origin: 50% 50%; /* Center the rotation */
+  width: 120px;
+  height: 120px;
+  display: block;
 
-    /* Apply rotation animation only to the group of elements */
-    .rotate-group {
-      animation: ${rotate} 8s linear infinite;
-      transform-origin: center;
-    }
-  `;
+  /* Apply rotation animation only to the group of elements */
+  .rotate-group {
+    animation: ${rotate} 8s linear infinite;
+    transform-origin: center;
+  }
+`;
--- src/component/ChatPopup.js
+++ src/component/ChatPopup.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
-import fssaiImage from "../Images/fssai.png";
-import botImg from "../Images/chaticon2.png";
+import botImg from "../assest/Images/chaticon2.png";
+import "../assest/css/popupstyle.css"; // Make sure this file contains all your CSS
+
 const queryParams = new URLSearchParams(window.location.search);
 const model = queryParams.get("model");
 
@@ -8,47 +9,49 @@ function ChatPopup({ isOpen, onClose }) {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
   const chatBoxRef = useRef(null);
+  const [inputValue, setInputValue] = useState("");
 
-  // Function to get current time formatted as HH:MM AM/PM
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value); // Update state with the input value
+  };
   const getCurrentTime = () => {
     const date = new Date();
     let hours = date.getHours();
     const minutes = date.getMinutes();
-    //const seconds = date.getSeconds(); // Get seconds
     const period = hours >= 12 ? "pm" : "am";
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     const minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
-    //const secondsFormatted = seconds < 10 ? `0${seconds}` : seconds; // Format seconds
-    //return `${hours}:${minutesFormatted}:${secondsFormatted} ${period}`;
     return `${hours}:${minutesFormatted} ${period}`;
   };
 
   // Automatically scroll to the bottom when a new message is added
   useEffect(() => {
-    // Scroll to the bottom of the chatbox whenever the messages state changes
     if (chatBoxRef.current) {
       chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
     }
   }, [messages]);
 
   useEffect(() => {
-    // Load a default image and message when the chatbot opens
     if (isOpen) {
       setMessages([
         {
           text: "Hello! Welcome to FSSAI! How can I assist you today?",
           sender: "Bot",
-          time: getCurrentTime(),
           buttons: [
             { text: "General Query", action: "generalQuery" },
-            { text: "Track Application", action: "trackApplication" },
+            {
+              text: "Track Application/Complaint/FBO",
+              action: "trackApplication",
+            },
           ],
+          time: getCurrentTime(),
         },
       ]);
     }
   }, [isOpen]);
-  const handleButtonClick = (action) => {
+
+  const handleButtonClick = (action, inputValue) => {
     if (action === "generalQuery") {
       setMessages((prev) => [
         ...prev,
@@ -62,23 +65,148 @@ function ChatPopup({ isOpen, onClose }) {
       setMessages((prev) => [
         ...prev,
         {
+          text: "Track Application/Complaint/FBO",
+          sender: "You", // Message from user
+          time: getCurrentTime(),
+        },
+        {
           text: "Please select one of the below options:",
           sender: "Bot",
-          time: getCurrentTime(),
           buttons: [
             { text: "Track Application", action: "TA" },
             { text: "Complaint Status", action: "Complaint Status" },
             { text: "Know Your Officer", action: "Know Your Officer" },
-            { text: "FBO Search", action: " FBO Search" },
+            { text: "FBO Search", action: "FBO Search" },
           ],
+          time: getCurrentTime(),
         },
       ]);
+    } else if (action === "TA") {
+      setMessages((prev) => [
+        ...prev,
+        {
+          text: "Please Enter your Application tracking id",
+          sender: "Bot",
+          input: [
+            {
+              placeholder: "Enter Your tracking Id",
+              type: "text",
+            },
+          ],
+          buttons: [
+            {
+              text: "Get Application Status",
+              action: "Get Status",
+              onClick: (index) => handleButtonClickTA("Get Status", index), // Pass index to the handler
+            },
+          ],
+          time: getCurrentTime(),
+        },
+      ]);
+    } else if (action === "Complaint Status") {
+      setMessages((prev) => [
+        ...prev,
+        {
+          text: "Please Enter Your Complaint No.",
+          sender: "Bot",
+          input: [
+            {
+              placeholder: "Enter Your Complaint No.",
+              type: "text",
+            },
+          ],
+          buttons: [
+            {
+              text: "Get Complaint Status",
+              action: "Get Complaint Status",
+            },
+          ],
+          time: getCurrentTime(),
+        },
+      ]);
+    } else if (action === "Get Status") {
+      handleButtonClickTA(action);
+    } else if (action === "Get Complaint Status") {
+      //handleButtonClickCS(action);
+    }
+  };
+  const handleButtonClickTA = (action) => {
+    if (action === "Get Status") {
+      const trackingIdValue = inputValue;
+      console.log(trackingIdValue);
+
+      if (!trackingIdValue) {
+        alert("Please enter your Application Tracking ID.");
+        return;
+      }
+
+      const apiUrl = `https://fctest.fssai.gov.in/gateway/commonauth/commonapi/gettrackapplicationdetails/${trackingIdValue}`;
+
+      fetch(apiUrl, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Failed to fetch application status.");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          const applicationData = data[0]; // Assuming the API returns an array and we need to access the first object
+
+          setMessages((prev) => [
+            ...prev,
+            {
+              text:
+                `Application Status: ${
+                  applicationData.statusDesc || "Unknown"
+                }\n` +
+                `Company Name: ${
+                  applicationData.companyName || "Not Available"
+                }\n` +
+                `License Category: ${
+                  applicationData.licenseCategoryName || "Not Available"
+                }\n` +
+                `Address: ${
+                  applicationData.addressPremises || "Not Available"
+                }\n` +
+                `District: ${
+                  applicationData.districtName || "Not Available"
+                }\n` +
+                `State: ${applicationData.stateName || "Not Available"}\n` +
+                `Pincode: ${
+                  applicationData.premisePincode || "Not Available"
+                }\n` +
+                `Application Type: ${
+                  applicationData.apptypeDesc || "Not Available"
+                }\n` +
+                `Submission Date: ${
+                  applicationData.appSubmissionDate || "Not Available"
+                }`,
+              sender: "Bot",
+              time: getCurrentTime(),
+            },
+          ]);
+        })
+        .catch((error) => {
+          console.log(error);
+          setMessages((prev) => [
+            ...prev,
+            {
+              text: `Error: Unable to retrieve the application status. Please try again later.`,
+              sender: "Bot",
+              time: getCurrentTime(),
+            },
+          ]);
+        });
     }
   };
 
   const handleSend = () => {
     if (input.trim()) {
-      // Add a new message from the sender (You)
       const currentTime = getCurrentTime();
       setMessages([
         ...messages,
@@ -92,23 +220,24 @@ function ChatPopup({ isOpen, onClose }) {
         setMessages((prevMessages) => [
           ...prevMessages,
           {
-            text: `Welcome to FSSAI! I am the Food Safety and Standards Authority of India (FSSAI) Health Assistant (FSHA) Chatbot, your personal assistant here to help with any FSSAI-related queries. Feel free to ask me more by choosing one of the options below. I am here to assist you on your export journey.   This is a static response. It is not provided by the chatbot because there is no API connected on the backend of the chatbot.  ${model}`,
+            text: `Welcome to FSSAI! I am the Food Safety and Standards Authority of India (FSSAI) Health Assistant (FSHA) Chatbot, your personal assistant here to help with any FSSAI-related queries. Feel free to ask me more by choosing one of the options below. I am here to assist you on your export journey. This is a static response. It is not provided by the chatbot because there is no API connected on the backend of the chatbot. ${model}`,
             sender: "Receiver",
             time: receiverTime,
           },
         ]);
-      }, 4000); // Add a delay for the receiver's response
+      }, 4000);
     }
   };
+
   return (
     isOpen && (
-      <div style={styles.popupContainer}>
+      <div className="popupContainer">
         <div></div>
-        <div style={styles.popupHeader}>
-          <div style={styles.greeting}>
+        <div className="popupHeader">
+          <div className="greeting">
             <p>Hi !</p>
           </div>
-          <div style={styles.description}>
+          <div className="description">
             <p>
               Welcome to FSSAI! I am Food Safety Health Assistant (FSHA)
               Chatbot, your personal assistant here to help with any
@@ -116,47 +245,63 @@ function ChatPopup({ isOpen, onClose }) {
               the options below. I am here to assist you on your export journey.
             </p>
           </div>
-          <button style={styles.closeButton} onClick={onClose}></button>
+          <button className="closeButton" onClick={onClose}></button>
         </div>
-
-        <div ref={chatBoxRef} style={styles.chatBox}>
+        <div ref={chatBoxRef} className="chatBox">
           {messages.map((message, index) => (
             <div
               key={index}
-              style={{
-                ...styles.messageContainer,
-                ...(message.sender === "You"
-                  ? styles.myMessageContainer
-                  : styles.theirMessageContainer),
-              }}
+              className={`messageContainer ${
+                message.sender === "You"
+                  ? "myMessageContainer"
+                  : "theirMessageContainer"
+              }`}
             >
-              {/* Sender's Image */}
               {message.sender !== "You" && (
                 <img
                   src={botImg}
                   alt={`${message.sender} avatar`}
-                  style={styles.avatar}
+                  className="avatar"
                 />
               )}
-              {/* Message Content */}
               <div
-                style={{
-                  ...styles.message,
-                  ...(message.sender === "You"
-                    ? styles.myMessage
-                    : styles.theirMessage),
-                }}
+                className={`message ${
+                  message.sender === "You" ? "myMessage" : "theirMessage"
+                }`}
               >
-                <span style={styles.text}>{message.text}</span>
-                <div style={styles.time}>{message.time}</div>
+                <span className="text">{message.text}</span>
+                <div className="time">{message.time}</div>
+
+                {message.input && (
+                  <div className="inputContainer">
+                    {message.input.map((inputField, idx) => (
+                      <div key={idx} className="inputWrapper">
+                        <input
+                          type={inputField.type}
+                          placeholder={inputField.placeholder}
+                          className="customInput"
+                          value={inputValue}
+                          onChange={handleInputChange}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                )}
 
                 {message.buttons && (
-                  <div style={styles.buttonsContainer}>
+                  <div className="buttonsContainer">
                     {message.buttons.map((button, idx) => (
                       <button
                         key={idx}
-                        onClick={() => handleButtonClick(button.action)}
-                        style={styles.button}
+                        onClick={() => {
+                          // Check if there is an input field above the button
+                          const inputField = document.querySelector(
+                            `#inputField-${idx}`
+                          );
+                          const inputValue = inputField ? inputField.value : "";
+                          handleButtonClick(button.action, inputValue);
+                        }}
+                        className="button"
                       >
                         {button.text}
                       </button>
@@ -167,16 +312,16 @@ function ChatPopup({ isOpen, onClose }) {
             </div>
           ))}
         </div>
-        <div style={styles.inputContainer}>
+        <div className="inputContainer">
           <input
             type="text"
-            style={styles.input}
+            className="input"
             placeholder="Type your message..."
             value={input}
             onChange={(e) => setInput(e.target.value)}
             onKeyPress={(e) => e.key === "Enter" && handleSend()}
           />
-          <button style={styles.buttonSend} onClick={handleSend}>
+          <button className="buttonSend" onClick={handleSend}>
             Send
           </button>
         </div>
@@ -184,183 +329,4 @@ function ChatPopup({ isOpen, onClose }) {
     )
   );
 }
-
-const styles = {
-  buttonsContainer: {
-    //display: "flex",
-    flexwrap: "wrap",
-  },
-  popupContainer: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    borderRadius: 0,
-    backgroundColor: "#fff",
-    display: "flex",
-    flexDirection: "column",
-    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-    zIndex: 999,
-  },
-
-  buttonContainer: {
-    display: "flex",
-    justifyContent: "space-around",
-    padding: "10px",
-    borderBottom: "1px solid #ccc",
-  },
-  optionButton: {
-    padding: "10px 15px",
-    border: "none",
-    borderRadius: "5px",
-    backgroundColor: "#ff5e04",
-    color: "white",
-    cursor: "pointer",
-    fontSize: "14px",
-  },
-  welcomeMessage: {
-    marginTop: "10px",
-    fontSize: "14px",
-    lineHeight: "1.5",
-    textAlign: "justify",
-  },
-  popupHeader: {
-    padding: "10px",
-    backgroundColor: "#ff5e04",
-    justifyContent: "space-between",
-    alignItems: "center",
-    position: "relative",
-    borderBottom: "1px solid #ccc",
-  },
-  greeting: {
-    fontSize: "14px",
-    fontWeight: "bold",
-    marginBottom: "10px",
-    color: "white",
-  },
-  description: {
-    fontSize: "13px",
-    lineHeight: "1.4",
-    marginBottom: "4px",
-    margin: "2px",
-    color: "white",
-  },
-  closeButton: {
-    position: "absolute",
-    top: "10px",
-    right: "10px",
-    backgroundColor: "transparent",
-    border: "none",
-    color: "#ffffff",
-    fontSize: "10px",
-    cursor: "pointer",
-    padding: "0",
-  },
-  chatBox: {
-    flex: 1,
-    overflowY: "auto",
-    backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.63), rgba(255, 255, 255, 0.8)), url(${fssaiImage})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    scrollbarWidth: "none",
-    msOverflowStyle: "none",
-  },
-  messageContainer: {
-    display: "flex",
-    alignItems: "flex-start",
-    margin: "10px 10px",
-  },
-  myMessageContainer: {
-    flexDirection: "row-reverse",
-  },
-  theirMessageContainer: {
-    flexDirection: "row",
-  },
-  avatar: {
-    width: "40px",
-    height: "50  px",
-    borderRadius: "50%",
-    margin: "0 5px",
-  },
-  message: {
-    padding: "5px 10px",
-    borderRadius: "10px",
-    maxWidth: "80%",
-    wordWrap: "break-word",
-    whiteSpace: "pre-wrap",
-    position: "relative",
-  },
-  myMessage: {
-    backgroundColor: "#d1ffd6",
-    textAlign: "left",
-  },
-  theirMessage: {
-    backgroundColor: "#dddddd",
-    textAlign: "left",
-  },
-  text: {
-    fontSize: "0.9em",
-    color: "#000",
-  },
-  time: {
-    fontSize: "0.6em",
-    color: "#777",
-    textAlign: "right",
-  },
-  inputContainer: {
-    display: "flex",
-    padding: "10px",
-    borderTop: "1px solid #ccc",
-  },
-  input: {
-    flex: 1,
-    padding: "10px",
-    border: "1px solid #ccc",
-    borderRadius: "5px",
-  },
-  buttonSend: {
-    padding: "10px 15px",
-    border: "none",
-    borderRadius: "5px",
-    backgroundColor: "#ff5e04",
-    color: "white",
-    cursor: "pointer",
-  },
-  // button: {
-  //   padding: "10px 15px",
-  //   backgroundColor: "white",
-  //   border: "1px solid #ff5e04",
-  //   color: "black",
-  //   borderRadius: "5px",
-  //   cursor: "pointer",
-  //   fontSize: "11px" /* Set the font size to 9px */,
-  //   transition: "background-color 0.3s, color 0.3s" /* Smooth transition */,
-  // },
-  button: {
-    padding: "8px 14px",
-    backgroundColor: "white",
-    border: "2px solid #ff5e04",
-    color: "black",
-    borderRadius: "4px",
-    cursor: "pointer",
-    fontSize: "11px",
-    transition: "background-color 0.3s, color 0.3s",
-    whiteSpace: "nowrap" /* Allows text to wrap inside the button */,
-    maxwidth: "150px" /* Optional: Limit button width */,
-    wordWrap: "break-word" /* Ensure the text wraps inside the button */,
-    marginBottom: "10px",
-    margin: "5px",
-  },
-  buttonHover: {
-    backgroundColor:
-      "#ff5e04" /* On hover, background color matches the border */,
-    color: "white" /* Text color changes to white on hover */,
-  },
-};
-
 export default ChatPopup;
